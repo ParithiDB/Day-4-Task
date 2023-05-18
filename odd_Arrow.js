f112897d4fd62b15b5789409e03a985e7b762fab
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-18 12:47:07 +0530

    Create odd_Arrow.js

---
+++ odd_Arrow.js
@@ -0,0 +1,15 @@
+        // Arrow Function
+
+var oddNum = (a) => {
+   for(let i = 0; i<a.length; i++)
+  {
+  		if(a[i] % 2 != 0)
+      {
+      		console.log( a[i]);
+      }
+  }
+};
+
+oddNum([3,7,3,5,2,8,5,2,3,4,6,33,43,23,46])
+
+// Output Link: https://jsfiddle.net/46bqhtkd/10/
