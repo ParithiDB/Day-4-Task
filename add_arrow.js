7938dafdc90f1a859a82ecc38ba34ede37e439e3
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-18 13:50:39 +0530

    Create add_arrow.js

---
+++ add_arrow.js
@@ -0,0 +1,14 @@
+                            // Arrow Function
+
+let add = (a) => {
+  let sum = 0;
+  
+  for(let i = 0;i<a.length;i++)
+  {
+  sum += a[i];
+  }
+  console.log(sum);
+};
+add([1,5,6,5,78,3,2])
+
+//Output Link: https://jsfiddle.net/46bqhtkd/17/
