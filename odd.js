21bb321c53265a098011fb9c6dacd22d615f67b1
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-18 12:18:43 +0530

    Update odd.js

--- odd.js
+++ odd.js
@@ -1,12 +1,12 @@
-function oddNum(a)
-{
-	for(let i = 0; i<a.length; i++)
+(function (a) {
+  for(let i = 0; i<a.length; i++)
   {
   		if(a[i] % 2 != 0)
       {
       		console.log( a[i]);
       }
   }
-}
+})([3,7,3,5,2,8,5,2,3,4,6,33,43,23,46]);
 
-oddNum([3,7,3,5,2,8,5,2,3,4,6,33,43,23,46]);
+
+Output Link: https://jsfiddle.net/46bqhtkd/6/ 
