d2bd8d3aaaa4dd842d6832c1d3b0dd0390db397d
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-18 12:35:42 +0530

    Update odd.js

--- odd.js
+++ odd.js
@@ -1,3 +1,6 @@
+            // IIFE
+
+
 (function (a) {
   for(let i = 0; i<a.length; i++)
   {
@@ -9,4 +12,22 @@
 })([3,7,3,5,2,8,5,2,3,4,6,33,43,23,46]);
 
 
-Output Link: https://jsfiddle.net/46bqhtkd/6/ 
+// Output Link: https://jsfiddle.net/46bqhtkd/8/
+
+
+
+                // Anonymous
+
+var fun1 = function (a) {
+ for(let i = 0; i<a.length; i++)
+  {
+  		if(a[i] % 2 != 0)
+      {
+      		console.log( a[i]);
+      }
+  }
+}
+
+fun1([3,7,3,5,2,8,5,2,3,4,6,33,43,23,46]);
+
+// Output Link: https://jsfiddle.net/46bqhtkd/6/
