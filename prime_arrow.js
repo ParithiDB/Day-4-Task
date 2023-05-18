ad39b1038a0d50663b4666fe5df3df3a1135180e
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-18 14:27:57 +0530

    Create prime_arrow.js

---
+++ prime_arrow.js
@@ -0,0 +1,36 @@
+              // Arrow Function
+
+
+
+
+
+var prime = (numbers) => {
+
+ function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+    
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  
+  var primeNumbers = [];
+  
+  for (var i = 0; i < numbers.length; i++) {
+    if (isPrime(numbers[i])) {
+      primeNumbers.push(numbers[i]);
+    }
+  }
+  
+  console.log(primeNumbers);
+ 
+};
+prime([1,2,3,4,5,6,7,8,9]);
+
+// Output Link: https://jsfiddle.net/46bqhtkd/26/
